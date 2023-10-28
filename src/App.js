f3ef869dcffdf4e78110fb177751f4876f0db29d
viktor0110 <viktor-stefanov@mail.bg>
2023-10-28 18:21:23 +0300

    nodemon  installed and app.css updated.

--- src/App.js
+++ src/App.js
@@ -8,7 +8,9 @@ function App() {
     return (
         <div className="App">
             <Header />
-            <HomePage />
+            <main className="main" id="main">
+                <HomePage />
+            </main>
             <Footer />
         </div>
     );
