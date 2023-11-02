d39ea8601465c193941174429f90d1fd78aebcf6
viktor0110 <viktor-stefanov@mail.bg>
2023-11-02 16:16:18 +0200

    Calendar implemented.

--- src/components/main/BookingPage.js
+++ src/components/main/BookingPage.js
@@ -1,16 +1,11 @@
 import styles from '../../styles/BookingPage.module.css'
 import Calendar from 'react-calendar';
-import 'react-calendar/dist/Calendar.css';
+import '../../styles/customCalendar.css';
 
 export default function BookingPage() {
     return (
         <section id="bookingPage" className={styles.bookingPage}>
-            <h1 className={styles.message}>TO DO WHEN LEARN REACT</h1>
-            <img className={styles.calendar} src="/images/calendarView.jpg" alt="calendar"/>
-            <div>
             <Calendar />
-
-            </div>
         </section>
     );
 };
