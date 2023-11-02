1a335e43b1dda303f2a31ff109a0a13bd6b1876e
viktor0110 <viktor-stefanov@mail.bg>
2023-11-02 17:55:44 +0200

    react-router implemented.

--- src/App.js
+++ src/App.js
@@ -8,19 +8,22 @@ import DefaultPage from './components/main/DefaultPage';
 import BookingPage from './components/main/BookingPage';
 import UploadPage from './components/main/UploadPage';
 import ContactPage from './components/main/ContactPage';
+import { Routes, Route } from 'react-router-dom';
 
 function App() {
     return (
         <>
             <Header />
             <main>
-                <HomePage />
-                <RegisterPage />
-                <LoginPage />
-                <DefaultPage />
-                <BookingPage />
-                <UploadPage />
-                <ContactPage />
+                <Routes>
+                    <Route path='/' element={<HomePage />} />
+                    <Route path='/register' element={<RegisterPage />} />
+                    <Route path='/login' element={<LoginPage />} />
+                    <Route path='/booking' element={<BookingPage />} />
+                    <Route path='/upload' element={<UploadPage />} />
+                    <Route path='/contact' element={<ContactPage />} />
+                    <Route path='*' element={<DefaultPage />} />
+                </Routes>
             </main>
             <Footer />
         </>
