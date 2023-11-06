5d289720e67086adff3cbfdb94629fdc219d7f9b
viktor0110 <viktor-stefanov@mail.bg>
2023-11-06 16:39:49 +0200

    submitHandler edited to sync func.

--- src/components/views/UploadPage.js
+++ src/components/views/UploadPage.js
@@ -7,12 +7,12 @@ export default function UploadPage() {
     const navigate = useNavigate();
 
     const uploadFileHandler = (e) => {
-        if(e.target.files[0].type !== "image/jpeg") {
+        if (e.target.files[0].type !== "image/jpeg") {
             //should reset input 
             alert('Only images are allowed');
             return;
         }
-        if(e.target.files[0].size > 5000000) {
+        if (e.target.files[0].size > 5000000) {
             //should reset input 
             alert('Images over 5MB are not allowed !');
             return;
@@ -20,29 +20,30 @@ export default function UploadPage() {
         setImage(e.target.files[0])
     };
 
-    async function submitHandler(e) {
+     function submitHandler(e) {
         e.preventDefault();
-        
+
         const formData = new FormData();
         formData.append('files', image);
 
-        await fetch('http://localhost:5000/data/upload', {
+        fetch('http://localhost:5000/data/upload', {
             method: 'POST',
             body: formData
-        });
+        })
+            .then(res => res.json())
+            .then(info => console.log(info))
+            .catch(err => console.log(err))
 
         navigate('/gallery');
     }
 
     return (
         <section id="uploadPage" className={styles.uploadPage}>
-    <form className={styles.uploadForm} onSubmit={submitHandler} >
-        <div>
-            <label htmlFor="files">Photo:</label>
-            <input className={styles['input-photo']} id="files" name="files" type="file" onChange={uploadFileHandler}/>
-        </div>
-        <button className={styles.btn} type="submit">Upload</button>
-    </form>
-</section>
+            <form className={styles.uploadForm} onSubmit={submitHandler} >
+                <label htmlFor="files">Photo:</label>
+                <input className={styles['input-photo']} id="files" name="files" type="file" onChange={uploadFileHandler} />
+                <button className={styles.btn} type="submit">Upload</button>
+            </form>
+        </section>
     );
 };
