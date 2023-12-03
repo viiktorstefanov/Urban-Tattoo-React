85b5805d52860d4ae9fadeebf4b5ab1f4b175503
ViiktorStefanov <victorystefanov@gmail.com>
2023-12-03 12:12:38 +0200

    styles for next and previous button in Calendar

--- src/components/BookingPage/BookingPage.js
+++ src/components/BookingPage/BookingPage.js
@@ -12,7 +12,7 @@ export default function BookingPage() {
     const [ values, setValues ] = useState({
         date: '',
     });
-
+    
     const onSubmit = async (e) => {
         e.preventDefault();
 
