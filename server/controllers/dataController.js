dd24f5c3d5b4ebe15c3d7cd75c18d668e51df2f9
Viktor Stefanov <victorystefanov@gmail.com>
2024-02-09 10:28:22 +0200

    test img folder on server

--- server/controllers/dataController.js
+++ server/controllers/dataController.js
@@ -21,8 +21,8 @@ dataController.post('/upload', isAdmin(), async (req, res) => {
         const uploadPath = path.join(imagesDir, imageName);
 
         // const uploadPath = path.join(path.resolve(__dirname, '../'), '/images', imageName);
-        console.log(uploadPath);
-        const imageUrl = `https://urban-eell.onrender.com/${imageName}`;
+       
+        const imageUrl = `https://urban-eell.onrender.com/images/${imageName}`;
     
         if (file.size > 5000000) {
             console.log('Cannot upload image bigger than 5MB');
