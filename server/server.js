d84a79bd7d5207c44e77c9eb8e5535b4bdb3d327
Viktor Stefanov <victorystefanov@gmail.com>
2024-02-10 18:25:46 +0200

    redeploy

--- server/server.js
+++ server/server.js
@@ -5,10 +5,11 @@ const expressConfig = require("./config/express");
 const routesConfig = require("./config/routes");
 const functions = require("firebase-functions");
 
-const app = express();
-const PORT = process.env.OUT_PORT || 3000;
 
 async function startServer() {
+  const app = express();
+  const PORT = process.env.OUT_PORT || 5000;
+
   await databaseConfig(app);
 
   expressConfig(app);
