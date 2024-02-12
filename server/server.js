d34f3bd7e4587e7cfcf73a1aabe02ef5de234c5d
Viktor Stefanov <victorystefanov@gmail.com>
2024-02-13 00:00:58 +0200

    vercel config

--- server/server.js
+++ server/server.js
@@ -4,12 +4,18 @@ const { databaseConfig } = require("./config/database");
 const expressConfig = require("./config/express");
 const routesConfig = require("./config/routes");
 
-const app = express();
-const PORT = process.env.OUT_PORT || 5000;
 
-await databaseConfig(app);
+async function startServer() {
+  const app = express();
+  const PORT = process.env.OUT_PORT || 5000;
 
-expressConfig(app);
-routesConfig(app);
+  await databaseConfig(app);
 
-app.listen(PORT, () => console.log(`Server listening on port: ${PORT}`));
+  expressConfig(app);
+  routesConfig(app);
+  
+  app.listen(PORT, () => console.log(`Server listening on port: ${PORT}`));
+
+}
+
+startServer();
