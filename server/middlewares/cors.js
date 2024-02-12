991d8e3b0b1ee6d48d6c2b85e8101a4f936e48a7
Viktor Stefanov <victorystefanov@gmail.com>
2024-02-12 20:25:41 +0200

    cors

--- server/middlewares/cors.js
+++ server/middlewares/cors.js
@@ -1,5 +1,5 @@
 module.exports = () => (req, res, next ) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Origin', 'https://urban-tattoo-react.web.app');
     res.setHeader('Access-Control-Allow-Methods', 'HEAD, OPTIONS, GET, POST, PUT, DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'Content-type, Urban-Authorization, user');
 
